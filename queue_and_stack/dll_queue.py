9064d15e2a22dce05031461bd107f32e5d5a13bd
Lindsey C <lindseyacason@gmail.com>
2020-01-28 16:28:11 -0600

    Queue done with notes, updated size although not needed

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -10,16 +10,20 @@ from doubly_linked_list import DoublyLinkedList
 
 class Queue:
     def __init__(self):
+        self.size=0
         self.storage=DoublyLinkedList()
         #THIS IS BRINGING IN EVERYTHING FROM DOUBLY LINKED LIST THAT WE MADE. WE CAN THEN USE THE METHODS WE WROTE TO ACCOMPLISH WHAT WE WANT WITH THE METHODS BELOW.
 
     def enqueue(self, value):
         #    * `enqueue` should add an item to the back of the queue.
         self.storage.add_to_tail(value) #CALLING ADD TO HEAD AND PASSING IN THE VALUE. ADD TO HEAD IN DLL WILL RUN AND ACCOMPLISH THIS TASK
+        self.size +=1 #increment size
 
     def dequeue(self):
         #    * `dequeue` should remove and return an item from the front of the queue.
         #value = self.storage.head #WE DON'T NEED THIS HERE BECAUSE IT'S TAKEN CARE OF IN THE REMOVE FROM HEAD FUNCTION IN DLL, THIS CAUSES AN ERROR. JUST RETURN THE CODE AND IT WILL RETURN THE VALUE THAT IS RETURNED FROM THE FUNCTION CALLED FROM DLL.
+        self.size -=1 #decrement size
+
         return self.storage.remove_from_head()
 
     def len(self):
