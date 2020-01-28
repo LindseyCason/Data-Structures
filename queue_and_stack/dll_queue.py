2fe26ffa97e76451ad7dc92559bdd4162addb5be
Lindsey C <lindseyacason@gmail.com>
2020-01-28 16:26:12 -0600

    Queue done

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -3,17 +3,25 @@ sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
+#  * Should have the methods: `enqueue`, `dequeue`, and `len`.
+#    * `enqueue` should add an item to the back of the queue.
+#    * `dequeue` should remove and return an item from the front of the queue.
+#    * `len` returns the number of items in the queue.
+
 class Queue:
     def __init__(self):
-        self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage=DoublyLinkedList()
+        #THIS IS BRINGING IN EVERYTHING FROM DOUBLY LINKED LIST THAT WE MADE. WE CAN THEN USE THE METHODS WE WROTE TO ACCOMPLISH WHAT WE WANT WITH THE METHODS BELOW.
 
     def enqueue(self, value):
-        pass
+        #    * `enqueue` should add an item to the back of the queue.
+        self.storage.add_to_tail(value) #CALLING ADD TO HEAD AND PASSING IN THE VALUE. ADD TO HEAD IN DLL WILL RUN AND ACCOMPLISH THIS TASK
 
     def dequeue(self):
-        pass
+        #    * `dequeue` should remove and return an item from the front of the queue.
+        #value = self.storage.head #WE DON'T NEED THIS HERE BECAUSE IT'S TAKEN CARE OF IN THE REMOVE FROM HEAD FUNCTION IN DLL, THIS CAUSES AN ERROR. JUST RETURN THE CODE AND IT WILL RETURN THE VALUE THAT IS RETURNED FROM THE FUNCTION CALLED FROM DLL.
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        #    * `len` returns the number of items in the queue.
+        return self.storage.length
