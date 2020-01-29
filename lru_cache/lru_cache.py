a4b7211c0b1d128a77ee4b01c88dc65423aea42f
Lindsey C <lindseyacason@gmail.com>
2020-01-29 14:33:41 -0600

    LRU finished, code cleaned, error in DLL fixed

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,12 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        #DLL to keep order, Dictonary to keep KV pair, Current Size and Limit
+        self.order = DoublyLinkedList()
+        self.storage = {} #ORDERED
+        self.size=0
+        self.limit=limit
+        
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +24,13 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.storage: #self.storage IS the DICT
+            node=self.storage[key]
+            self.order.move_to_end(node)#MOST RECENTLY USED
+            return node.value[1]#0 is they key 1 is the value, make sure you're returning value.
+        else:
+            return None
+
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +43,21 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        #lecture notes
+        #Create node if key not found and move to front, increase size
+        #Move node to front if key IS found because recently used
+        #If full, remove last node from BOTH linked list AND dictionary
+        if key in self.storage:
+            node=self.storage[key] #This is a pointer to the key
+            node.value=(key,value) #the value is the tuple for that key/-->updating
+            self.order.add_to_tail(node) #makes it most recent
+            return
+
+        if self.size == self.limit:
+            del self.storage[self.order.head.value[0]] #REMOVED FROM DICT
+            self.order.remove_from_head() #REMOVE IT FROM DLL
+            self.size -= 1
+
+        self.order.add_to_tail((key,value)) #If size = 0 then add key,value TUPLE
+        self.storage[key] =self.order.tail
+        self.size += 1
